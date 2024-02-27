213890e5489d7e53083b97e7b7370e9c7d212b80
Rafael Lopez Gonzalez <rlopgon274@g.educaand.es>
2024-02-27 19:46:39 +0100

    REFACTORIZACIÓN DE NOMBRES

--- src/main/java/org/example/Triangulo.java
+++ src/main/java/org/example/Triangulo.java
@@ -4,26 +4,26 @@ public class Triangulo {
     private static Scanner teclado;
     public static void main(String[] args) {
         //@author Rafael Lopez Gonzalez
-        double l1, l2, l3;
+        double Lado1, Lado2, Lado3;
         String t= "";
         teclado = new Scanner(System.in);
         System.out.println("Introduce el primer lado del triángulo:");
-            l1 = teclado.nextDouble();
+            Lado1 = teclado.nextDouble();
         System.out.println("Introduce el segundo lado del triángulo:");
-            l2 = teclado.nextInt();
+            Lado2 = teclado.nextInt();
         System.out.println("Introduce el tercer lado del triángulo:");
-            l3 = teclado.nextFloat();
-        if (l1 <= 0 || l2 <= 0 || l3 <= 0) {
+            Lado3 = teclado.nextFloat();
+        if (Lado1 <= 0 || Lado2 <= 0 || Lado3 <= 0) {
             System.out.println("Los lados de un triángulo deben ser positivos.");
-        } else if (l1 == l2 && l2 == l3) {
+        } else if (Lado1 == Lado2 && Lado2 == Lado3) {
             t="equilátero";
-        } else if (l1 == l2 || l1 == l3 || l2 == l3) {
+        } else if (Lado1 == Lado2 || Lado1 == Lado3 || Lado2 == Lado3) {
             t="isósceles";
         } else {
             t="escaleno";
         }
-        double perimetro = l1 + l2 +l3;
-        System.out.println("El triángulo es "+ t + " y su perímetro es: " + perimetro);
+        double PerimetroDelTriangulo = Lado1 + Lado2 +Lado3;
+        System.out.println("El triángulo es "+ t + " y su perímetro es: " + PerimetroDelTriangulo);
         teclado.close();
     }
 }
